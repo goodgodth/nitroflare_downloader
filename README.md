ff3ee18276eb7ed164493082360e87fcfef00150
arruk204 <arruk204@exten.postbox.in.th>
2020-09-25 18:05:24 +0700

    change parameter , update README.md

--- README.md
+++ README.md
@@ -6,3 +6,32 @@ python nitroflaredl.py {USERNAME-premiumuser} {PASSWORD} {SAVE TO DIRECTORY} {FU
 
 Example
 python nitroflaredl.py noeybnk48<span>@<span>gmail.com noeyBNK48 /home/noeybnk48/download/ /home/noeybnk48/dl.txt
+
+
+
+
+# Nitroflare_Downloader
+Download file from rapidgator with premium user
+
+## nitroflaredl.py
+Download by read list of URL in text file (Example : dl.txt)
+
+### How to run
+
+```sh
+$ python nitroflaredl.py {USERNAME-premiumuser} {PASSWORD} {FULL_PATH_OF_FILE_dl.txt} {FULL_PATH_OF_SAVE_TO_DIRECTORY}
+```
+
+### Example
+```sh
+$ python nitroflaredl.py noeybnk48@gmail.com noeyBNK48 /home/noeybnk48/dl.txt /home/noeybnk48/download/
+```
+
+###
+
+---
+
+
+### Reference
+
+ - [Nitroflare API](https://nitroflare.com/member?s=general-api).
--- nitroflaredl.py
+++ nitroflaredl.py
@@ -2,17 +2,21 @@ import requests
 import os
 import sys
 
-URL = "https://nitroflare.com/api/v2/getKeyInfo"
+NTFURL_KEYINFO = "https://nitroflare.com/api/v2/getKeyInfo"
+NTFURL_FILEINFO = "https://nitroflare.com/api/v2/getFileInfo"
+NTFURL_DOWNLOADLINK = "https://nitroflare.com/api/v2/getDownloadLink"
+
 
 PARAMS = {"user": sys.argv[1], "premiumKey": sys.argv[2]}
 
-PATH = sys.argv[3];
+LISTOFDOWNLOAD = sys.argv[3]
+PATH = sys.argv[4];
 
-r = requests.get(url=URL, params=PARAMS)
+r = requests.get(url=NTFURL_KEYINFO, params=PARAMS)
 data = r.json()
 print("Premium Account : " + data["type"])
 
-with open(sys.argv[4]) as fp:
+with open(LISTOFDOWNLOAD) as fp:
     line = fp.readline()
     cnt = 1
     while line:
@@ -20,9 +24,9 @@ with open(sys.argv[4]) as fp:
         _url = line.strip()
         _file_id = _url.split("/")[4]
         print(_file_id)
-        url = "https://nitroflare.com/api/v2/getFileInfo"
+        
         params = {"files": _file_id}
-        r = requests.get(url=url, params=params)
+        r = requests.get(url=NTFURL_FILEINFO, params=params)
         data = r.json()
         print("getFileInfo : " + str(data))
         print(
@@ -31,10 +35,10 @@ with open(sys.argv[4]) as fp:
             + " -> "
             + data["result"]["files"][_file_id]["status"]
         )
-        url = "https://nitroflare.com/api/v2/getDownloadLink"
+        
         params = PARAMS
         params["file"] = _file_id
-        r = requests.get(url=url, params=params)
+        r = requests.get(url=NTFURL_DOWNLOADLINK, params=params)
         data = r.json()
         print("getDownloadLink : " + str(data))
         _dl_url = data["result"]["url"]
